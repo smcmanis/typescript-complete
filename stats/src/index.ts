b242a11c5bc8cf840d748792a3e7e8e60732e3f4
smcmanis <simon.mcmanis@gmail.com>
2023-01-01 15:12:03 +1100

    Delegate everything within classes + static methods

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,23 +1,9 @@
-import { CsvFileReader } from './inheritance/CsvFileReader';
 import { MatchReader } from './inheritance/MatchReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
 
-const reader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(reader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
-const matches = matchReader.matches;
 
-// Variable named after a specific team
-let manUnitedWins = 0;
-
-// Analysis type is fixed
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-// No ability to output the report in different formats
-console.log(`Man United won ${manUnitedWins} games`);
+Summary.winsAnalysisWithHtmlReport('Liverpool').buildAndPrintReport(
+  matchReader.matches
+);
--- stats/src/inheritance/MatchReader.ts
+++ stats/src/inheritance/MatchReader.ts
@@ -1,8 +1,7 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from '../MatchResult';
+import { MatchData } from '../MatchData';
 import { dateStringToDate } from '../utils';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+import { CsvFileReader } from './CsvFileReader';
 
 interface DataReader {
   read(): void;
@@ -10,6 +9,10 @@ interface DataReader {
 }
 
 export class MatchReader {
+  static fromCsv(filename: string): MatchReader {
+    return new MatchReader(new CsvFileReader(filename));
+  }
+
   matches: MatchData[] = [];
 
   constructor(public reader: DataReader) {}
