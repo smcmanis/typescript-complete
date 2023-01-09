6e51d46f3bf152f378cbf2f6164ce99e8de0efcc
smcmanis <simon.mcmanis@gmail.com>
2023-01-09 20:23:29 +1100

    Convert User to a Reusable Model class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import { User } from './models/User';
 
-const user = new User({ id: 4 });
+const user = User.buildUser({ id: 4 });
 
 user.fetch();
 
 setTimeout(() => console.log(user), 4000);
-
-// setTimeout(() => {
-//   console.log(user);
-// }, 4000);
