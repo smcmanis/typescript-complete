35ea88e7886b53b09b6c07ebb6eaec258d4f2915
smcmanis <simon.mcmanis@gmail.com>
2023-01-13 18:49:00 +1100

    Add generic collection class

---
+++ web/src/models/Collection.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+import { Model } from './Model';
+import { User } from './User';
+
+export class Collection<T, K> {
+  models: T[] = [];
+  events: Eventing = new Eventing();
+
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse): void => {
+      response.data.forEach((value: K): void => {
+        this.models.push(this.deserialize(value));
+      });
+
+      this.trigger('change');
+    });
+  }
+}
