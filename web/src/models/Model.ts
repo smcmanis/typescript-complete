a231129651b40ab638cf875ee3d21bb6726d6b8b
smcmanis <simon.mcmanis@gmail.com>
2023-01-13 18:35:51 +1100

    Use shortened passthrough syntax

--- web/src/models/Model.ts
+++ web/src/models/Model.ts
@@ -27,22 +27,16 @@ export class Model<T extends HasId> {
     private sync: Sync<T>
   ) {}
 
-  get get() {
-    return this.attributes.get;
-  }
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
     this.events.trigger('change');
   }
 
-  get on() {
-    return this.events.on;
-  }
+  on = this.events.on;
 
-  get trigger() {
-    return this.events.trigger;
-  }
+  trigger = this.events.trigger;
 
   fetch(): void {
     const id = this.get('id');
